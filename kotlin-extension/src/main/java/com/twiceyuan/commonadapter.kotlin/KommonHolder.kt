dfb24f00c5434a80128c3f5cdf2e4a95b2951b17
twiceYuan <twiceyuan@gmail.com>
2018-11-02 11:26:51 +0800

    rename parameter name

--- kotlin-extension/src/main/java/com/twiceyuan/commonadapter.kotlin/KommonHolder.kt
+++ kotlin-extension/src/main/java/com/twiceyuan/commonadapter.kotlin/KommonHolder.kt
@@ -4,9 +4,10 @@ import android.view.View
 import com.twiceyuan.commonadapter.library.holder.CommonHolder
 
 abstract class KommonHolder<T> : CommonHolder<T>() {
-    override fun bindData(t: T) {
-        itemView.bindData(t)
+
+    override fun bindData(data: T) {
+        itemView.bindData(data)
     }
 
-    abstract fun View.bindData(t: T)
+    abstract fun View.bindData(data: T)
 }
